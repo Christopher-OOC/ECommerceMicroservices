90c63096a4c140b4a97e0748326803dbc8af786e
Christopher-OOC <olojedechristopher24@gmail.com>
2025-04-20 21:58:17 +0100

    commit

---
+++ services/Customer/src/main/java/com/javalord/Customer/customer/CustomerController.java
@@ -0,0 +1,23 @@
+package com.javalord.Customer.customer;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+
+
+    }
+}
