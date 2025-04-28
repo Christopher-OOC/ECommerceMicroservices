ebfcb555325149400a554b604a4785728a9c948f
Christopher-OOC <olojedechristopher24@gmail.com>
2025-04-28 11:09:32 +0100

    commit

--- services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailService.java
+++ services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailService.java
@@ -1,19 +1,26 @@
 package com.javalord.Notification.notification.email;
 
+import com.javalord.Notification.notification.kafka.order.Product;
+import com.javalord.Notification.notification.kafka.payment.PaymentConfirmation;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender mailSender;
@@ -29,9 +36,61 @@ public class EmailService {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
         mimeMessageHelper.setFrom("olojedechristopher24@gmail.com");
-        final String templateName =
+        final String templateName = EmailTemplates.PAYMENT_CONFIRMATION.getTemplate();
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("amount", amount);
+        variables.put("orderReference", orderReference);
 
+        Context context = new Context();
+        context.setVariables(variables);
+        mimeMessageHelper.setSubject(EmailTemplates.PAYMENT_CONFIRMATION.getSubject());
 
+        try {
+            String htmlTemplate = templateEngine.process(templateName, context);
+            mimeMessageHelper.setText(htmlTemplate, true);
+            mimeMessageHelper.setTo(destinationEmail);
+
+            mailSender.send(mimeMessage);
+            log.info("Email successfully sent");
+        }
+        catch (MessagingException ex) {
+            log.warn("Error in sending email");
+        }
+    }
+
+    @Async
+    public void sendOrderConfirmationEmail(
+            String destinationEmail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference,
+            List<Product> products
+    ) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
+        mimeMessageHelper.setFrom("olojedechristopher24@gmail.com");
+        final String templateName = EmailTemplates.PAYMENT_CONFIRMATION.getTemplate();
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("customerName", customerName);
+        variables.put("amount", amount);
+        variables.put("orderReference", orderReference);
+
+        Context context = new Context();
+        context.setVariables(variables);
+        mimeMessageHelper.setSubject(EmailTemplates.PAYMENT_CONFIRMATION.getSubject());
+
+        try {
+            String htmlTemplate = templateEngine.process(templateName, context);
+            mimeMessageHelper.setText(htmlTemplate, true);
+            mimeMessageHelper.setTo(destinationEmail);
+
+            mailSender.send(mimeMessage);
+            log.info("Email successfully sent");
+        }
+        catch (MessagingException ex) {
+            log.warn("Error in sending email");
+        }
     }
 
 }
--- services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailTemplates.java
+++ services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailTemplates.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 public enum EmailTemplates {
 
-    EMAIL_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
+    PAYMENT_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
     ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation");
 
     @Getter
