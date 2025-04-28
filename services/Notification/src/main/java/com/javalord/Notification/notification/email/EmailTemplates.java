8b68b023ba6005278b7e86bc9446eaa97452dbc9
Christopher-OOC <olojedechristopher24@gmail.com>
2025-04-28 10:30:06 +0100

    dkjhvdbnm

--- services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailTemplates.java
+++ services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailTemplates.java
@@ -1,8 +1,19 @@
 package com.javalord.Notification.notification.email;
 
+import lombok.Getter;
+
 public enum EmailTemplates {
 
     EMAIL_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
-    ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation"),
+    ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation");
 
+    @Getter
+    private final String template;
+    @Getter
+    private final String subject;
+
+    EmailTemplates(String template, String subject) {
+        this.template = template;
+        this.subject = subject;
+    }
 }
