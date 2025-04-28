65304598cda6adc596458e352ed039d4e3628495
Christopher-OOC <olojedechristopher24@gmail.com>
2025-04-28 10:24:09 +0100

    commit

--- services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailService.java
+++ services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailService.java
@@ -1,9 +1,37 @@
 package com.javalord.Notification.notification.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendPaymentSuccessEmail(
+            String destinationEmail,
+            String customerName,
+            BigDecimal amount,
+            String orderReference
+    ) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, MimeMessageHelper.MULTIPART_MODE_RELATED, StandardCharsets.UTF_8.name());
+        mimeMessageHelper.setFrom("olojedechristopher24@gmail.com");
+        final String templateName =
+
+
+    }
+
 }
---
+++ services/Notification/src/main/java/com/javalord/Notification/notification/email/EmailTemplates.java
@@ -0,0 +1,8 @@
+package com.javalord.Notification.notification.email;
+
+public enum EmailTemplates {
+
+    EMAIL_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
+    ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation"),
+
+}
